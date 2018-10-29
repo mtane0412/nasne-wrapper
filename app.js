6d66dc3d203e0afec0d46842c3f97c5f2a795ff4
mtane0412 <mtane0412@gmail.com>
2018-10-29 17:54:29 +0900

    format

--- app.js
+++ app.js
@@ -19,57 +19,57 @@ const result = {
 
 let newVideo = false; // 新着録画フラグ
 
-function nasnePost(nasneOptions){
-		/*
-		 * nasneから録画番組一覧を取得
-		 * 新着録画がある場合はGASにpost
-		 */
+function nasnePost(nasneOptions) {
+	/*
+	 * nasneから録画番組一覧を取得
+	 * 新着録画がある場合はGASにpost
+	 */
 
-		request(nasneOptions).then(function (body) {
-				body.item.forEach(function(key){
-						if(moment().diff(moment(key.startDateTime), 'hours') < 2) newVideo = true;
-						let program = {
-								id: key.id,
-								title: key.title
-								.replace(/\ue195/g, '[終]')
-								.replace(/\ue193/g, '[新]')
-								.replace(/\ue0fe/g, '[字]')
-								.replace(/\uE184/g, '[解]')
-								.replace(/\uE183/g, '[多]')
-								.replace(/\uE180/g, '[デ]'),
-								description: key.description,
-								startDateTime: key.startDateTime,
-								duration: key.duration
-						};
-						result.item.push(program);
-				}); 
-				if (newVideo) {
-						const options = {
-								uri: process.env.GAS_URL,
-								method: 'POST',
-								headers: {
-										"Content-Type": "application/json",
-								},
-								body: JSON.stringify(result)
-						};
-						request(options, function(error, response, body){});
-						console.log('Update has been sent.', new Date());
-				} else {
-						console.log('No Update', new Date());
-				}
-		}).catch(function (err) {
-				console.log(err);
+	request(nasneOptions).then(function (body) {
+		body.item.forEach(function (key) {
+			if (moment().diff(moment(key.startDateTime), 'hours') < 2) newVideo = true;
+			let program = {
+				id: key.id,
+				title: key.title
+					.replace(/\ue195/g, '[終]')
+					.replace(/\ue193/g, '[新]')
+					.replace(/\ue0fe/g, '[字]')
+					.replace(/\uE184/g, '[解]')
+					.replace(/\uE183/g, '[多]')
+					.replace(/\uE180/g, '[デ]'),
+				description: key.description,
+				startDateTime: key.startDateTime,
+				duration: key.duration
+			};
+			result.item.push(program);
 		});
+		if (newVideo) {
+			const options = {
+				uri: process.env.GAS_URL,
+				method: 'POST',
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(result)
+			};
+			request(options, function (error, response, body) { });
+			console.log('Update has been sent.', new Date());
+		} else {
+			console.log('No Update', new Date());
+		}
+	}).catch(function (err) {
+		console.log(err);
+	});
 }
 
 // 毎時10分に番組チェック
 const cronTime = "10 * * * *";
 
 const job = new cronJob({
-		cronTime: cronTime,
-		onTick: nasnePost(nasneOptions),
-		start: false,
-		timezone: "Japan/Tokyo"
+	cronTime: cronTime,
+	onTick: nasnePost(nasneOptions),
+	start: false,
+	timezone: "Japan/Tokyo"
 })
 
 job.start();
