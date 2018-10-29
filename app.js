59019e10f068722bb7a71fbf9cb532bba5dea62d
mtane0412 <mtane0412@gmail.com>
2018-10-29 18:25:17 +0900

    bug fix

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const cronJob = require('cron').CronJob;
 const moment = require('moment');
 
 const nasneOptions = {
-	url: 'http://" + process.env.NASNE_ADDRESS + ":64220/recorded/titleListGet?searchCriteria=0&filter=0&startingIndex=0&requestedCount=0&sortCriteria=0&withDescriptionLong=0&withUserData=0',
-	method: 'GET',
+	url: "http://" + process.env.NASNE_ADDRESS + ":64220/recorded/titleListGet?searchCriteria=0&filter=0&startingIndex=0&requestedCount=0&sortCriteria=0&withDescriptionLong=0&withUserData=0",
+	method: "GET",
 	json: true
 }
 
@@ -29,12 +29,12 @@ function nasnePost(nasneOptions) {
 			let program = {
 				id: key.id,
 				title: key.title // 特殊文字を置き換え
-					.replace(/\ue195/g, '[終]')
-					.replace(/\ue193/g, '[新]')
-					.replace(/\ue0fe/g, '[字]')
-					.replace(/\uE184/g, '[解]')
-					.replace(/\uE183/g, '[多]')
-					.replace(/\uE180/g, '[デ]'),
+					.replace(/\ue195/g, "[終]")
+					.replace(/\ue193/g, "[新]")
+					.replace(/\ue0fe/g, "[字]")
+					.replace(/\uE184/g, "[解]")
+					.replace(/\uE183/g, "[多]")
+					.replace(/\uE180/g, "[デ]"),
 				description: key.description,
 				startDateTime: key.startDateTime,
 				duration: key.duration
@@ -44,7 +44,7 @@ function nasnePost(nasneOptions) {
 		if (newVideo) {
 			const options = {
 				uri: process.env.GAS_URL,
-				method: 'POST',
+				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
 				},
