56b7fabfa6c71373fac40210f909fdd7cf43c897
mtane0412 <mtane0412@gmail.com>
2018-10-31 23:15:17 +0900

    add cron

--- index.js
+++ index.js
@@ -5,5 +5,17 @@ const postToGAS = require('./postToGAS.js');
 
 const nasne = new Nasne('192.168.11.2');
 
-nasne.titleListGet(postToGAS);
-nasne.HDDInfoGet(postToGAS);
+const CronJob = require('cron').CronJob;
+
+const job_titleListGet = new CronJob('0 10 * * * *', function() {
+    console.log(`${new Date()} cronjob: titleListGet`);
+    nasne.titleListGet(postToGAS);
+});
+
+const job_HDDInfoGet = new CronJob('0 30 23 * * *', function() {
+    console.log(`${new Date()} cronjob: HDDInfoGet`);
+    nasne.HDDInfoGet(postToGAS);
+});
+
+job_titleListGet.start();
+job_HDDInfoGet.start();
