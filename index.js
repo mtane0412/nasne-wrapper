f69e1b6ffa779a08def8050defb19b4d5db745c5
mtane0412 <mtane0412@gmail.com>
2018-11-01 14:56:12 +0900

    update

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const nasne = new Nasne('192.168.11.2');
 const CronJob = require('cron').CronJob;
 
 const job_titleListGet = new CronJob('0 10 * * * *', function() {
-    console.log(`${new Date()} cronjob: titleListGet`);
     nasne.titleListGet(postToGAS);
 });
 
 const job_HDDInfoGet = new CronJob('0 30 23 * * *', function() {
-    console.log(`${new Date()} cronjob: HDDInfoGet`);
     nasne.HDDInfoGet(postToGAS);
 });
 
