5afdafbf5eb12315c19e7bed686cc48f65eb96a3
mtane0412 <mtane0412@gmail.com>
2018-11-07 17:24:44 +0900

    update

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const postToGAS = require('./postToGAS.js');
 
 const nasne = new Nasne('192.168.11.2');
 
-nasne.eventRelayInfoGet(console.log);
 
-/* 
 const CronJob = require('cron').CronJob;
 
 const job_titleListGet = new CronJob('0 10 * * * *', function () {
@@ -20,4 +18,3 @@ const job_HDDInfoGet = new CronJob('0 30 23 * * *', function () {
 
 job_titleListGet.start();
 job_HDDInfoGet.start();
-*/
